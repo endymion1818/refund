293cba6870f94f0515b52e751208ed9604738ce4
endymion1818 <endymion1818@gmail.com>
2022-01-11 15:08:37 +0000

    allow any origin

--- index.js
+++ index.js
@@ -36,13 +36,16 @@ const mockOrder = {
 }
 
 app.get('/', (req, res) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
     return res.send('ok')
 });
 
 app.get('/:orderid', (req, res) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
     return res.send(JSON.stringify(mockOrder))
 });
 
 app.listen(process.env.PORT, () => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
     console.log(`app listening on ${process.env.PORT}`);
 })
